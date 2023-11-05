89d07edfe6f30687df3629804058c8258b5bbc65
Stefan Veselinov <chofexx@gmail.com>
2023-11-05 08:42:03 +0200

    Adding component

--- src/app/modules/home-page/home-page.module.ts
+++ src/app/modules/home-page/home-page.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './home-page.component';
+import { HomeComponent } from 'src/app/components/home/home-component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
